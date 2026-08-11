885e040bc73b450bf1ada5cd8146e2b65f429edc
Robiul Hasan Jisan <robiulhasanjisan88@gmail.com>
2026-08-11 10:36:56 +0600

    Delete Language_Learning-main directory

--- Language_Learning-main/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from "@/components/shared/loading-spinner";
-
-export default function GlobalLoading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <LoadingSpinner size="lg" />
-    </div>
-  );
-}
